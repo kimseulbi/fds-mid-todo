2ee0d892b26da8522f61730c8e96331952896c8e
kimseulbi <ksb5166@gmail.com>
2018-11-05 17:34:02 +0900

    할일추가 기능

--- src/index.js
+++ src/index.js
@@ -1,38 +1,99 @@
 import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL:'https://tender-move.glitch.me/'})
+const api = axios.create({
+  baseURL: "https://tender-move.glitch.me/"
+});
+
+// Axios Interceptor - 그때그때 다른 설정 사용하기
+// axios에는 매번 요청이 일어나기 직전에 **설정 객체를 가로채서** 원하는대로 편집할 수 있는 기능이 있습니다.
+api.interceptors.request.use(function(config) {
+  // localStorage에 token이 있으면 요청에 헤더 설정, 없으면 아무것도 하지 않음
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers["Authorization"] = "Bearer " + token;
+  }
+  return config;
+});
 
 const templates = {
-  loginForm: document.querySelector('#login-form').content
-}
+  loginForm: document.querySelector("#login-form").content,
+  todoList: document.querySelector("#todo-list").content,
+  todoItem: document.querySelector("#todo-item").content
+};
 
-const rootEl = document.querySelector('.root')
+const rootEl = document.querySelector(".root");
 
 function drawLoginForm() {
   // 1. 템플릿 복사하기
-  const fragment = document.importNode(templates.loginForm, true)
+  const fragment = document.importNode(templates.loginForm, true);
+
   // 2. 내용 채우고, 이벤트 리스너 등록하기
-  const loginFormEl = fragment.querySelector('.login-form')
-  // 비동기?
-  loginFormEl.addEventListener('submit',async e => {
-    // alert('로그인 요청 전송')
-    e.preventDefault()
+  const loginFormEl = fragment.querySelector(".login-form");
+
+  loginFormEl.addEventListener("submit", async e => {
+    e.preventDefault();
     // e: 이벤트 객체
     // e.target: 이벤트를 실제로 일으킨 요소 객체 (여기서는 loginFormEl)
-    // e.target.elements: 폼 내부에 들어있는 요소 객체를 편하세 가져올 수 있는 특이한 개체
-    // e.target.elements.usernmae: name 어트리뷰트에 username이라고 지정되어 있는 input 요소 객체
+    // e.target.elements: 폼 내부에 들어있는 요소 객체를 편하게 가져올 수 있는 특이한 객체
+    // e.target.elements.username: name 어트리뷰트에 username이라고 지정되어있는 input 요소 객체
     // .value: 사용자가 input 태그에 입력한 값
-    const username = e.target.elements.username.value
+    const username = e.target.elements.username.value;
     const password = e.target.elements.password.value;
-    const res = await api.post('/users/login', {
+    const res = await api.post("/users/login", {
       username,
       password
-    })
-    alert(res.data.token)
-  })
+    });
+    localStorage.setItem("token", res.data.token);
+    drawTodoList();
+  });
+
   // 3. 문서 내부에 삽입하기
-  rootEl.appendChild(fragment)
+  rootEl.textContent = "";
+  rootEl.appendChild(fragment);
 }
 
-drawLoginForm()
+async function drawTodoList() {
+  const res = await api.get("/todos");
+  const list = res.data;
+  // 1. 템플릿 복사하기
+  const fragment = document.importNode(templates.todoList, true);
+
+  // 2. 내용 채우고 이벤트 리스너 등록하기
+  const todoListEl = fragment.querySelector(".todo-list");
+  const todoFormEl = fragment.querySelector(".todo-form");
+  const deleteButtonEl = fragment.querySelector(".delete -button");
+
+  todoFormEl.addEventListener("submit", async e => {
+    e.preventDefault();
+    const body = e.target.elements.body.value;
+    const res = await api.post("/todos", {
+      body,
+      complete: false
+    });
+    if (res.status === 201) {
+      drawTodoList();
+    }
+    // drawTodoList();
+  });
+
+  list.forEach(todoItem => {
+    // 1. 템플릿 복사하기
+    const fragment = document.importNode(templates.todoItem, true);
+
+    // 2. 내용 채우고 이벤트 리스너 등록하기
+    const bodyEl = fragment.querySelector(".body");
+
+    bodyEl.textContent = todoItem.body;
+
+    // 3. 문서 내부에 삽입하기
+    todoListEl.appendChild(fragment);
+  });
+
+  // 3. 문서 내부에 삽입하기
+  rootEl.textContent = "";
+  rootEl.appendChild(fragment);
+}
+
+drawLoginForm();
